80796a27c912cc3e9db11a6fa48ca551cb60a5b1
snsolomar <snsolomar@gmail.com>
2023-11-29 14:11:02 -0800

    initialize fleet

--- battleShip.cpp
+++ battleShip.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+const int BOARD_SIZE = 10, FLEET_SIZE=3;
 struct Point {
     int row;
     int col;
@@ -15,12 +15,25 @@ struct Ship {
     // the size of the ship, the super-sipplied starting location and orientation
     vector<Point> location; 
 };
+struct Player {
+    char board[BOARD_SIZE][BOARD_SIZE];
+    Ship fleet[FLEET_SIZE];
+};
+
+void initFleet(Player p){
+    // populate a players fleet
+    // 1. Read ships.txt, read the name and the size
+    /*for each line of file
+        select the next ship in the array, s
+        read the name, s.name
+        read the size, s.size
+        */
+}
 
 int main() {
     
 
-    // populate a players fleet
-    // 1. Read ships.txt, read the name and the size
+
 
 
 }
