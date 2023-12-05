7fe4de17c16b57aac0f433c95be6a80d0eeddf21
snsolomar <snsolomar@gmail.com>
2023-12-05 14:26:27 -0800

    lab

--- lab.cpp
+++ lab.cpp
@@ -16,9 +16,16 @@ class Student {
         int age;
 };
 
+void Student::output()
+{
+    cout << "Student name is " << name << " age is " << age << "!" << endl; 
+}
+
 int main()
 {
     Student s1, s2("Michael");
+
+    s1.setName("Natalie");
 
     return 0;
 }
